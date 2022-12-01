46668f3d49ef619a6b5def8a28b81f6f82d73f39
Chris Margonis <chris.margonis@withplum.com>
2022-12-01 23:59:03 +0200

    Solve Day 1 AOC

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
+    fun getElves(input: List<String>): List<Elf> {
+        var calories = 0
+        val elves = mutableListOf<Elf>()
+        input.forEach {
+            if (it.isNotBlank()) {
+                calories += it.toInt()
+            } else {
+                elves.add(Elf(calories))
+                calories = 0
+            }
+        }
+        return elves
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = getElves(input)
+        return elves.maxBy { it.totalCalories }.totalCalories
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>): Int = getElves(input)
+        .asSequence()
+        .sortedByDescending { it.totalCalories }
+        .take(3)
+        .sumOf { it.totalCalories }
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
+data class Elf(val totalCalories: Int)
