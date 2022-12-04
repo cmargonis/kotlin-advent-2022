753431e1f309a8034c4d907d69f1e0e1575618e0
Chris Margonis <chris.margonis@withplum.com>
2022-12-04 23:37:45 +0200

    Solve Day 4 AOC

---
+++ src/Day04.kt
@@ -0,0 +1,31 @@
+fun main() {
+
+    fun IntRange.contains(other: IntRange): Boolean = first <= other.first && last >= other.last
+
+    /**
+     * Converts e.g. 22-34 to a [IntRange]
+     */
+    fun textToRange(rawRange: String): IntRange = rawRange
+        .split("-")
+        .map { it.toInt() }
+        .zipWithNext { a, b -> a..b }
+        .first()
+
+    fun groupToRanges(it: String) = it.split(",").map { range -> textToRange(range) }
+
+    fun part1(input: List<String>): Int = input.map {
+        groupToRanges(it)
+            .zipWithNext { a: IntRange, b: IntRange -> a.contains(b) or b.contains(a) }
+            .first()
+    }.count { it }
+
+    fun part2(input: List<String>): Int = input.map {
+        groupToRanges(it)
+            .zipWithNext { a: IntRange, b: IntRange -> a.intersect(b).isNotEmpty() }
+            .first()
+    }.count { it }
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
